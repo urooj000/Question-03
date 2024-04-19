2eaaf14a5efbc045b031fd2939b3d6fbbaea0dca
urooj22 <160038688+urooj000@users.noreply.github.com>
2024-04-19 19:46:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName = "urooj sajjad";
+console.log(personName.toLocaleLowerCase());
+console.log(personName.toLocaleUpperCase());
+console.log(personName.charAt(0).toLocaleUpperCase() + personName.slice(1));
---
+++ main.ts
@@ -0,0 +1,7 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName: string = "urooj sajjad";
+
+console.log(personName.toLocaleLowerCase());
+console.log(personName.toLocaleUpperCase());
+console.log(personName.charAt(0).toLocaleUpperCase() + personName.slice(1));
